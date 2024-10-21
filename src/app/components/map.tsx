fedc22b76c2acfc41221e385b80c134f5336ebe4
albores339 <juandediosalboreshernandez@gmail.com>
2024-10-20 18:37:22 -0600

    más detalles

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -14,7 +14,7 @@ const Map: React.FC = () => {
     <section className="my-16">
       <h2 className="text-2xl md:text-4xl font-bold text-cyan-700 text-center mb-4 md:mb-8">Nuestra Ubicación</h2>
       {isMapVisible && (
-        <div className="w-full h-96 mx-auto max-w-screen-xl p-4">
+        <div className="w-full h-96 mx-auto max-w-screen-xl px-4">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.3036668955924!2d-93.13256132484915!3d16.761560984022786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd9ba264bfae5%3A0x3244176b94a09529!2sAma%20ser%20el%20cambio%20AC!5e0!3m2!1ses-419!2smx!4v1727149076334!5m2!1ses-419!2smx"
             title="Mapa de la ubicación de Ama Ser el Cambio"
